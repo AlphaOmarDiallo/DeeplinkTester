0e61f76ba91beef587bfcc8b258e69ea33dfa8dc
Alpha <adiallo@exomind.fr>
2023-01-05 22:37:54 +0100

    Theme modified

--- app/src/main/java/com/alphaomardiallo/deeplinktester/ui/theme/Color.kt
+++ app/src/main/java/com/alphaomardiallo/deeplinktester/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.alphaomardiallo.deeplinktester.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue900 = Color(0xFF0000d0)
+val Purple50 = Color(0xFFeee5fc)
+val Purple400 = Color(0xFF7845ee)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Grey100 = Color(0xFFe9e9e9)
--- app/src/main/java/com/alphaomardiallo/deeplinktester/ui/theme/Theme.kt
+++ app/src/main/java/com/alphaomardiallo/deeplinktester/ui/theme/Theme.kt
@@ -16,15 +16,16 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue900,
+    secondary = Purple50,
+    tertiary = Purple400
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue900,
+    secondary = Purple50,
+    tertiary = Purple400,
+    background = Grey100
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
